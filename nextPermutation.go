3fa66c40c9b12df7e67b55cfa8836146b9a2f528
Jeffrey Neely <jeffneely@oningroup.com>
2022-06-02 08:13:16 -0500

    plus one

--- nextPermutation.go
+++ nextPermutation.go
@@ -1,36 +1,37 @@
 package main
 
 func nextPermutation(nums []int) {
-	l := len(nums)
-	if l < 1 {
+	ln := len(nums)
+	if ln < 1 || nums == nil {
 		return
 	}
 
-	lo := l - 2
+	lo := ln - 2
 	for lo >= 0 && nums[lo] >= nums[lo+1] {
 		lo--
 	}
 
-	if lo > 0 {
-		hi := l - 1
+	if lo >= 0 {
+		hi := ln - 1
 		for nums[hi] <= nums[lo] {
 			hi--
 		}
 		swap(&nums, lo, hi)
 	}
-	reverse(&nums, lo+1, l-1)
+	reverse(&nums, lo+1)
 }
 
-func swap(arr *[]int, i int, j int) {
-	tmp := (*arr)[i]
-	(*arr)[i] = (*arr)[j]
-	(*arr)[j] = tmp
-}
-
-func reverse(arr *[]int, i int, j int) {
+func reverse(arr *[]int, i int) {
+	j := len(*arr)
 	for i < j {
 		swap(arr, i, j)
 		i++
 		j--
 	}
+}
+
+func swap(arr *[]int, i, j int) {
+	tmp := (*arr)[i]
+	(*arr)[i] = (*arr)[j]
+	(*arr)[j] = tmp
 }
