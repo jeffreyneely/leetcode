d0178408557f0edf909cadd2d426797f715f916a
Jeffrey Neely <jeffneely@oningroup.com>
2022-06-02 08:13:33 -0500

    plus one

---
+++ plusOne.go
@@ -0,0 +1,16 @@
+package main
+
+func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] == 9 {
+			digits[i] = 0
+		} else {
+			digits[i]++
+			return digits
+		}
+	}
+	res := []int{}
+	res = append(res, 1)
+	res = append(res, digits...)
+	return res
+}
